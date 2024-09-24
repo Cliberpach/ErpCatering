f68a94a9d60616e8898708aba34f5a8759b2188f
luisdanielalva <luisdaniel9986@gmail.com>
2024-09-24 01:07:30 -0500

    fix

--- database/seeders/ProductoSeeder.php
+++ database/seeders/ProductoSeeder.php
@@ -19,7 +19,6 @@ class ProductoSeeder extends Seeder
         $producto->marca_id         =   3;
         $producto->unidad_medida_id =   54;
         $producto->precio           =   29.50;
-        $producto->stock            =   1;
         $producto->stock_minimo     =   1;
         $producto->save();
     }
