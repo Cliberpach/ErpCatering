1c96efc405dd08dcf158f3911fc2377019716c56
luisdanielalva <luisdaniel9986@gmail.com>
2024-09-24 01:40:46 -0500

    fix

--- app/Http/Controllers/Consultas/CProductoController.php
+++ app/Http/Controllers/Consultas/CProductoController.php
@@ -31,18 +31,18 @@ class CProductoController extends Controller
         $almacen_id     =   $request->get('almacen_id',null);
 
         $consulta = DB::table('kardex as k')
-                    ->join('productos as p', 'p.id', '=', 'k.producto_id')
-                    ->join('almacenes as a','a.id','k.almacen_id')
-                    ->select(
-                        'p.id as producto_id',
-                        'p.nombre as producto_nombre',
-                        'a.proyecto_id',
-                        DB::raw('MIN(k.stock_previo) as stock_inicial'),
-                        DB::raw('MAX(k.stock_posterior) as stock_final'),
-                        DB::raw('SUM(CASE WHEN k.registro_compra_id IS NOT NULL THEN k.cantidad ELSE 0 END) as ingreso'),
-                        DB::raw('SUM(CASE WHEN k.registro_salida_id IS NOT NULL THEN k.cantidad ELSE 0 END) as salida')
-                    )
-                    ->groupBy('p.id', 'p.nombre','a.proyecto_id');
+                                ->join('productos as p', 'p.id', '=', 'k.producto_id')
+                                ->join('almacenes as a', 'a.id', '=', 'k.almacen_id')
+                                ->select(
+                                    'p.id as producto_id',
+                                    'p.nombre as producto_nombre',
+                                    'a.proyecto_id',
+                                    DB::raw('(SELECT stock_previo FROM kardex WHERE producto_id = k.producto_id AND almacen_id = k.almacen_id ORDER BY fecha ASC LIMIT 1) as stock_inicial'),
+                                    DB::raw('(SELECT stock_posterior FROM kardex WHERE producto_id = k.producto_id AND almacen_id = k.almacen_id ORDER BY fecha DESC LIMIT 1) as stock_final'),
+                                    DB::raw('SUM(CASE WHEN k.registro_compra_id IS NOT NULL THEN k.cantidad ELSE 0 END) as ingreso'),
+                                    DB::raw('SUM(CASE WHEN k.registro_salida_id IS NOT NULL THEN k.cantidad ELSE 0 END) as salida')
+                                )
+                                ->groupBy('p.id', 'p.nombre', 'a.proyecto_id');
 
         if ($almacen_id) {
             $consulta->where('k.almacen_id', $almacen_id);
