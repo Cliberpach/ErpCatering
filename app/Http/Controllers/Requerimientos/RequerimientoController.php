e26b8fe6c4312be6b5024ecfff94d21df65965de
LuisDaniel <luisdaniel9986@gmail.com>
2024-10-12 14:01:15 -0500

    test

--- app/Http/Controllers/Requerimientos/RequerimientoController.php
+++ app/Http/Controllers/Requerimientos/RequerimientoController.php
@@ -138,7 +138,7 @@ class RequerimientoController extends Controller
 
             //======= ENVIAR MENSAJE AL SOCKET PARA QUE MUESTRE ALERTA DE NUEVO REQUERIMIENTO A LOS DE LOGÍSTICA ======
             $socketUrl = env('SOCKET_URL', 'http://localhost:3000'); 
-            dd($socketUrl);
+            dd(env('SOCKET_URL'));
             $client = new \GuzzleHttp\Client();
             $response   =   $client->post("$socketUrl/mensaje", [
                                 'json' => [ 
