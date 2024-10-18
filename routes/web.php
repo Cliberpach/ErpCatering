587a037d4db0e59455d3a2ad7cfe488b20af7646
LuisDaniel <luisdaniel9986@gmail.com>
2024-10-17 19:32:46 -0500

    test

--- routes/web.php
+++ routes/web.php
@@ -205,17 +205,7 @@ Route::group(['prefix' => 'tablas_generales_detalles', 'middleware' => ['auth','
 
 });
 
-Route::group(['prefix' => 'conductores', 'middleware' => ['auth','checkCustomPermission:registros.conductor','notificacionMiddleware']], function () {
-     
-    Route::get('/index', [ConductorController::class, 'index'])->name('registros.conductor.index');
-    Route::get('/create', [ConductorController::class, 'create'])->name('registros.conductor.create');
-    Route::get('/edit/{id}', [ConductorController::class, 'edit'])->name('registros.conductor.edit');
-    Route::post('/store', [ConductorController::class, 'store'])->name('registros.conductor.store');
-    Route::put('/update/{id}', [ConductorController::class, 'update'])->name('registros.conductor.update');
-    Route::get('/getConductores', [ConductorController::class, 'getConductores'])->name('registros.conductor.getConductores');
-    Route::delete('/destroy/{id}', [ConductorController::class, 'destroy'])->name('registros.conductor.destroy');
 
-});
 
 //============= FIN REGISTROS ==========================
 
